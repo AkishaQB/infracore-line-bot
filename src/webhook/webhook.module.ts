d0ff98a9db7522bf9c9a5ff20591edad51501269
Akisha <akisha@qburst.com>
2026-05-18 14:46:37 +0530

    fix crash

--- src/webhook/webhook.module.ts
+++ src/webhook/webhook.module.ts
@@ -3,9 +3,11 @@ import { WebhookController } from './webhook.controller';
 import { WebhookService } from './webhook.service';
 import { LineService } from '../line/line.service';
 import { UsersService } from '../users/users.service';
+import { SessionsService } from '../sessions/sessions.service';
+import { TicketsService } from '../tickets/tickets.service';
 
 @Module({
   controllers: [WebhookController],
-  providers: [WebhookService, LineService, UsersService],
+  providers: [WebhookService, LineService, UsersService, SessionsService, TicketsService],
 })
 export class WebhookModule {}
