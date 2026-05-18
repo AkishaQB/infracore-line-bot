7cd13d611b6b7366b526e58ec32fbc25a99911f0
Akisha <akisha@qburst.com>
2026-05-18 15:19:09 +0530

    downgrade

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import type { User } from '../../generated/prisma/client';
+import type { User } from '@prisma/client';
 import { LineProfile } from '../line/interfaces/line-profile.interface';
 
 @Injectable()
